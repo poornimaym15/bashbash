c419d15f425e385cd377abc64cd9e59e9e938dee
poornimaym15@gmail.com <poornimaym15@gamil.com>
2022-02-02 04:21:42 +0530

    this is gitbash2

---
+++ src/sdet_gitbash1/gitbashtest2.java
@@ -0,0 +1,10 @@
+package sdet_gitbash1;
+
+public class gitbashtest2 {
+
+	public static void main(String[] args) {
+		System.out.println("share this project to github via gitbash cmd line");
+
+	}
+
+}
